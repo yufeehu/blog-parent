635d098d6cf727653787598cc2fbebca0361551c
huyuhui <1913715845@qq.com>
2022-03-18 10:26:07 +0800

    新建项目

---
+++ blog-api/src/main/java/com/hyh/blog/BlogApiApplication.java
@@ -0,0 +1,16 @@
+package com.hyh.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author huyuhui
+ */
+@SpringBootApplication
+public class BlogApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApiApplication.class, args);
+    }
+
+}
---
+++ blog-api/src/test/java/com/hyh/blog/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyh.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
